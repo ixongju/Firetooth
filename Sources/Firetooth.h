9d0ef31576defde8bb43c41b6c642231c64b8639
ixongju <ixongju@gmail.com>
2019-06-25 23:53:56 +0900

    initial commit

---
+++ Sources/Firetooth.h
@@ -0,0 +1,19 @@
+//
+//  Firetooth.h
+//  Firetooth
+//
+//  Created by XIONGJU on R 1/06/25.
+//  Copyright © Reiwa 1 XIONG JU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Firetooth.
+FOUNDATION_EXPORT double FiretoothVersionNumber;
+
+//! Project version string for Firetooth.
+FOUNDATION_EXPORT const unsigned char FiretoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Firetooth/PublicHeader.h>
+
+
